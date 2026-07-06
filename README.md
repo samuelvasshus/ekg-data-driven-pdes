5f279abd217629ede5237a6a61458a140fdd4499
samuelvasshus <121942393+samuelvasshus@users.noreply.github.com>
2026-07-06 11:48:43 +0200

    fixed dimentions forier_least_square_info

---
+++ README.md
@@ -0,0 +1,11 @@
+Log
+
+Implementerte forier ting:
+*får ikke like mange frekvenser som
+ fourier koefisienter.
+*Må ta hensyn til dette i 
+ Least Square Methode mtp nyquist
+ har med komplekskonjugert å gjøre
+ og siden reelt signal vil 
+ komplekskonjugert bare bli kopi. 
+ Derfor bruke rfft og rfftfreq for samme lengde. 
--- src/ekgpde/fourier.py
+++ src/ekgpde/fourier.py
@@ -1,4 +1,14 @@
 import numpy as np
 
-def Forier_Least_square_info(time_series):
-    return np.fft.fft(time_series), 2*np.pi*np.fft.rfftfreq(time_series)
+
+def forier_least_square_info(
+    time_series: np.ndarray,
+    sample_rate: float = 128.0,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Compute forier coefficients and correspnding angular frequencies
+    Real signal, therefore rfft and rfftfreq with same length.
+    Takes care of nyquist frequency. 
+
+    """
+    return np.fft.rfft(time_series), 2*np.pi*np.fft.rfftfreq(len(time_series), 1/sample_rate)
